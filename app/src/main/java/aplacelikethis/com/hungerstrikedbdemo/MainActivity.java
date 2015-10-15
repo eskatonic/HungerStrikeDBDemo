fe709f7c49d499f5dd99beafa4aa56aebfe5ee54
BRIAN MAYS <brianmays@me.com>
2015-10-14 20:32:05 -0700

    Fixed Menu rendering issue - wbm

--- app/src/main/java/aplacelikethis/com/hungerstrikedbdemo/MainActivity.java
+++ app/src/main/java/aplacelikethis/com/hungerstrikedbdemo/MainActivity.java
@@ -2,10 +2,10 @@ package aplacelikethis.com.hungerstrikedbdemo;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
